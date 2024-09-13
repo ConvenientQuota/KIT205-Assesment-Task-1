cbec340bb8e89f41a26a122ed3c874c70c90eee0
Henry Croft <crofthans@yahoo.com.au>
2024-09-13 19:15:09 +1000

    zoo header file to store data structures

---
+++ Project1/zoo.h
@@ -0,0 +1,32 @@
+
+#define TableSize 10000
+
+/* Defining data structures */
+
+typedef struct Animal {
+	int animalID;
+	char name[20];
+	char species[20];
+	int age;
+	struct Animal* next; //Chaining in the linked list
+}Animal;
+
+typedef struct Enclosure {
+	int enclosureID;
+	char enclosureName[20];
+	Animal* animals; // the Linked list of all the animals within the enclosure
+	struct Enclosure* next; //Chaining for the hash table
+}Enclosure;
+
+typedef struct HashTable {
+	Enclosure* table[TableSize];
+} HashTable;
+
+/* Mapping an enclosureID to a hash table index*/
+int hashFunction(int enclosureID) {
+	return enclosureID % TableSize;
+}
+
+void insertEnclosure(HashTable* zoo, int enclosureID, char* name);
+void insertAnimal(HashTable* zoo, int enclosureID, int animalID, char* name, char* species, int age);
+
