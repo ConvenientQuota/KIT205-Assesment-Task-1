3bc7d679fbc6081a725bf1b438430004fda0f2e4
Henry Croft <crofthans@yahoo.com.au>
2024-09-13 18:59:10 +1000

    hashFunction and insertEnclosure functions

--- Project1/Chaining.c
+++ Project1/Chaining.c
@@ -24,3 +24,24 @@ typedef struct Enclosure {
 typedef struct HashTable {
 	Enclosure* table[TableSize];
 } HashTable;
+
+/* Mapping an enclosureID to a hash table index*/
+int hashFunction(int enclosureID) {
+	return enclosureID % TableSize;
+}
+
+/* function inserting new enclosure into hashtable */
+
+void insertEnclosure(HashTable* zoo, int enclosureID, char* name) {
+	int index = hashFunction(enclosureID); // Calculating the hash table index for the enclosureID
+	Enclosure* newEnclosure = (Enclosure*)malloc(sizeof(Enclosure)); // Allocating memory for the enclosure
+	newEnclosure->enclosureID = enclosureID; // setting the enclosureID of the new enclosure
+	strcpy(newEnclosure->enclosureName, name); // copying the enclosure name in the new enclosures name field
+	newEnclosure->animals = NULL; // initialising the animal linked list to NULL cause its a new enclosure
+	newEnclosure->next = zoo->table[index]; // Setting the next pointer of the new enclosure to the current head of the list at the index and implementing chaining in case of collision
+	zoo->table[index] = newEnclosure; //inserting the new enclosure at the beginning of the lsit in the table at given index
+}
+
+void insertAnimal(HashTable* zoo, int enclosureID, int animalID, char* name, char* species, int age) {
+
+}
